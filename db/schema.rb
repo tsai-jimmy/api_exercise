3db45882a14433b2dce73d9a41d317eaec32a5ba
JimmyTsai <qaz3182@gmail.com>
2020-01-13 20:53:04 +0800

    add gem rest-client and model city

---
+++ db/schema.rb
@@ -0,0 +1,26 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_01_13_124837) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string "juhe_id"
+    t.string "province"
+    t.string "city"
+    t.string "district"
+    t.string "current_temp"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["juhe_id"], name: "index_cities_on_juhe_id"
+  end
+
+end
